9ace5d1f9e85e8e7d2daa1b57a7de65c46958e09
Maria Sapiy <mariasapiy999@gmail.com>
2020-04-14 20:05:15 +0300

    added src

---
+++ src/main/java/ua/lviv/iot/dataAccess/ChemicalsRepo.java
@@ -0,0 +1,11 @@
+package ua.lviv.iot.dataAccess;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import ua.lviv.iot.springLabSeven.secondRest.model.HouseholdChemical;
+
+@Repository
+public interface ChemicalsRepo extends JpaRepository<HouseholdChemical, Integer> {
+
+}
